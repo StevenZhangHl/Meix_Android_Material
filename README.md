0d5ee70cf34ac453cb4683aca43d0ff7ae3108a2
zhanghl <15921327326@163.com>
2021-12-27 10:03:10 +0800

    编辑md文件

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Meix_Android_Material
 
 android组件、工具类库
+
+* CircleProgressBar
+* DragView
+* ShapeTextView
+* StepsView
---
+++ meixlibrary/src/main/java/com/meix/library/ui/TitleBar.java
@@ -0,0 +1,28 @@
+package com.meix.library.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * author steven
+ * date 2021/12/27
+ * description 通用标题栏
+ */
+public class TitleBar extends RelativeLayout {
+    public TitleBar(Context context) {
+        super(context);
+    }
+
+    public TitleBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TitleBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    private void init(){
+
+    }
+}
